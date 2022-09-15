f49003a5e959076d095fe05cc7810b2febb3571e
RedSparkie <36616604+RedSparkie@users.noreply.github.com>
2022-09-15 19:32:32 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+echo "# privatesd" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/RedSparkie/privatesd.git
+git push -u origin main
